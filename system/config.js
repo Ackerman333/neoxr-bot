231d0c91de7b623d5d4aea92bd1bf74ac5441341
Levi kun <106715427+Ackerman333@users.noreply.github.com>
2023-05-12 07:44:11 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,8 +1,8 @@
 const { Function, Scraper } = new (require('@neoxr/neoxr-js'))
 // Owner number
-global.owner = '6285221100126'
+global.owner = '628227557688'
 // Owner name
-global.owner_name = 'Wildan Izzudin'
+global.owner_name = 'IRJA'
 // Database name (Default: database)
 global.database = 'database'
 // Maximum upload file size limit (Default : 100 MB)
@@ -26,7 +26,7 @@ global.timezone = 'Asia/Jakarta'
 // Bot version
 global.version = '2.2.2',
 // Bot name
-global.botname = `© neoxr-bot v${global.version} (Public Bot)`
+global.botname = `© IRJA${global.version} (Public Bot)`
 // Footer text
 global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
 // Scraper
